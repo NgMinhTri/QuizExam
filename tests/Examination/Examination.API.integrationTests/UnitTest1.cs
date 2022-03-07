4732d3c7d0f510192f75ff5c2aba5ed443296b5e
Minh Tri Nguyen <minhtripro2903@gmail.com>
2022-03-07 13:34:23 +0700

    Add project files.

---
+++ tests/Examination/Examination.API.integrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Examination.API.integrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
