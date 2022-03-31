1faf6d6bfb96a2476fe2d6bc8856e3217868df7e
Minh Tri Nguyen <minhtripro2903@gmail.com>
2022-03-31 11:40:53 +0700

    Đăng nhập, đăng xuất cho Portal App

--- src/WebApps/PortalApp/Startup.cs
+++ src/WebApps/PortalApp/Startup.cs
@@ -1,13 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.IdentityModel.Logging;
+using PortalApp.Core;
 
 namespace PortalApp
 {
@@ -23,12 +20,37 @@ namespace PortalApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpContextAccessor();
+
             services.AddRazorPages();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = AuthenticationConsts.SignInScheme;
+                options.DefaultChallengeScheme = AuthenticationConsts.OidcAuthenticationScheme;
+            })
+            .AddCookie(AuthenticationConsts.SignInScheme, options =>
+            {
+                options.Cookie.Name = Configuration["IdentityServerConfig:CookieName"];
+                options.LoginPath = "/login.html";
+            })
+            .AddOpenIdConnect(AuthenticationConsts.OidcAuthenticationScheme, options =>
+            {
+                options.Authority = Configuration["IdentityServerConfig:IdentityServerUrl"];
+                options.ClientId = Configuration["IdentityServerConfig:ClientId"];
+                options.ClientSecret = Configuration["IdentityServerConfig:ClientSecret"];
+
+                options.ResponseType = "code";
+                options.RequireHttpsMetadata = false;
+                options.SaveTokens = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            IdentityModelEventSource.ShowPII = true;
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -39,11 +61,13 @@ namespace PortalApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
